ae2607fd4971600f91ae44cbd99415d7d8f3538f
labarilem <18102614+labarilem@users.noreply.github.com>
2025-01-26 17:56:45 +0100

    centered submit text

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,6 +1,6 @@
 export default function Submit() {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto text-center">
       <h1 className="text-4xl font-bold mb-4">Submit a Game</h1>
       <p className="text-gray-400 mb-8">
         Have you found or created a game that was featured on Hacker News?
